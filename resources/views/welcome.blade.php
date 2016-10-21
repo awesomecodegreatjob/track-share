80b22951710b858fe1338925c5091109ac821743
Maxx Crawford <mcrawford@broco.com>
2016-10-21 11:35:37 -0500

    Added homepage crap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,22 +8,33 @@
 
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/normalize.min.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.min.css">
+     <link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Montserrat|Oswald" rel="stylesheet">
      <link rel="stylesheet" href="/style.css" media="screen" charset="utf-8">
+     <link rel="stylesheet" href="/css/app.css" media="screen" charset="utf-8">
      <link rel="stylesheet" href="/fontello.css" media="screen" charset="utf-8">
 </head>
 <body>
 
-   <div class="row">
-      <div class="small-4 small-offset-4">
-         <form action="search">
-            <label for="url">Add Your Music URL <i class="icon-link"></i><br/><br/></label>
-            <input type="text" name="url" placeholder="Google Music or Spotify link">
-            <br/><br/>
-            <input type="submit" class="button small expand" value="SHARE">
-         </form>
+    <header>
+        <h1>Track Share</h1>
+    </header>
+    <main>
+        <div class="row">
+            <div class="small-4 small-offset-4">
+                <form action="search">
+                    <label for="url">Add Your Music URL <i class="icon-link"></i><br/><br/></label>
+                    <input type="text" name="url" placeholder="Google Music or Spotify link">
+                    <br/><br/>
+                    <input type="submit" class="button small expand" value="SHARE">
+                </form>
 
-      </div>
-   </div>
+            </div>
+        </div>
+    </main>
+
+   <footer>
+       &copy; 2016 Awesome Code Great Job. All rights reserved.
+   </footer>
 
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation.min.js"></script>
